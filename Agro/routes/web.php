9c2f4bc93f2f99614771651800e9c9ef3f9445e2
Rafael <rafaprimak1@gmail.com>
2024-09-27 20:34:02 -0300

    first commit

---
+++ Agro/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+
+Route::post('/products', [ProductController::class, 'store']);
+
+Route::get('/products/{id}/edit', [ProductController::class, 'edit'])->where('id', '[0-9]+');
+
+Route::put('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+');
+
+Route::patch('/products/{id}', [ProductController::class, 'update'])->where('id', '[0-9]+');
+
+Route::delete('/products/{id}', [ProductController::class, 'destroy'])->where('id', '[0-9]+');
+
+
