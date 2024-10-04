6fc5587f10105df2d0ddae4b181584d6e8a1a859
Rafael <rafaprimak1@gmail.com>
2024-10-04 20:00:40 -0300

    Projeto 04/10

--- Agro/routes/web.php
+++ Agro/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/products', [ProductController::class, 'index']);
 
